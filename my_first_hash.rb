b3de55c53a7a223b58f8362291079564cc4a406f
Steven Senkowski <scsenkowski@gmail.com>
2017-11-13 17:30:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,15 @@
 def my_hash
-  my_family = ["my_wife" => "Hannah", "my_brother" => "Alec", "my_dad" => "Curt", "my_mom" => 
+  my_family = ["my_wife" => "Hannah", "my_brother" => "Alec", "my_dad" => "Curt", "my_mom" =>
   "Cathy"]
-  
+
 end
 
 
 def shipping_manifest
+  shipping_manifest = ["whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3]
   #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
@@ -21,11 +22,8 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
+
   #your code here
   #remember to return the shipping_manifest hash
 
 end
-
-
-
